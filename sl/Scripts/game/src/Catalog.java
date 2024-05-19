9b6b3f85fb70c09c6fd82a71f6132ae7227356ad
Rodrigo Serrano <rodrigooh.dl@gmail.com>
2024-05-19 15:53:07 -0300

    Inital commit

--- sl/Scripts/game/src/Catalog.java
+++ sl/Scripts/game/src/Catalog.java
@@ -478,7 +478,7 @@ public class Catalog extends GameType implements GameState
 		//always visible objects:
 //		moneytxt=osd.createText( null, Frontend.mediumFont, Text.ALIGN_CENTER,	0.50, -0.94 );
 		moneytxt = osd.createText(null, Frontend.largeFont_strong, Text.ALIGN_CENTER,	0.50, -0.975);
-		moneytxt.setColor(Palette.RGB_BLACK);
+//		moneytxt.setColor(Palette.RGB_BLACK);
 		sectionstxt = osd.createText(null, Frontend.largeFont_strong, Text.ALIGN_LEFT, -0.98, -0.98); //RAXAT: v2.3.1, additional informer for sections
 //		sectionstxt.setColor(Palette.RGB_RED);
 		osd.endGroup();
